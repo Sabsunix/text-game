749349923143d7e18fca036ce7fb387ebe73af20
Sabsunix <39288059+Sabsunix@users.noreply.github.com>
2018-05-22 10:40:21 -0700

    Update 5/21/18

--- isuckatthis.py
+++ isuckatthis.py
@@ -26,7 +26,7 @@ def game_over():
         'Oof.',
         'Nice one, dude!',
     ]
-    
+
     print("")
     print(random.choice(quips))
     print("You freakin' died!")
@@ -60,7 +60,7 @@ def intro():
     # cont() replacement
     lol = input('*')
     lol = lol.lower()
-    
+
     # 'Getting mad at tutorial' easter egg
     if not retried and (lol == 'shut up' or lol == 'screw off'):
         print("Alright, fine. Geez.")
@@ -68,12 +68,12 @@ def intro():
     elif lol != "":
         print("(You're not supposed to type anything here)")
         print('')
-    
+
     print("It\'s dark. A single dimly lit bulb hangs from the ceiling -- the room\'s only light source.")
     cont()
     print("You're dizzy. Someone must have drugged you.")
     cont()
-    print("You sit up and take a look around. You\'re in a hospital room.")
+    print("You sit up and look around. You\'re in a hospital room.")
     cont()
     print("There is a door across the room.")
 
@@ -93,7 +93,7 @@ def intro():
         'falcon punch glass',
         'falcon punch window',
         'falcon punch door glass',
-        'falcon punch door window',            
+        'falcon punch door window',
     ]
 
     crowbar = [
@@ -115,28 +115,66 @@ def intro():
         'inject drugs',
         'inject needles',
         'do drugs',
+        'take drugs',
+        'take needles',
+    ]
+
+    throw_drugs = [
+        'throw drugs',
+        'throw needles',
+        'throw needle',
+        'smash window with needle',
+        'smash window with drugs',
+        'smash door window with needle',
+        'smash door window with drugs',
+        'break window with drugs',
+        'break window with needle',
+        'break door window with needle',
+        'break door window with drugs',
+        'break glass with drugs',
+        'break glass with needle',
+        'break door glass with drugs',
+        'break door glass with needle',
     ]
 
     # Player input fail count
     ifc = 0
 
+    # Tracks if player got up from bed from trying to unlock door.
+    # Just for a tiny dialogue nitpick.
+    got_up = False
+
     # First player action
     while True:
-        print("")
-        choice1 = input("What do you want to do? ")
+        print('')
+        choice1 = input('What do you want to do? ')
         choice1 = choice1.lower()
 
         # Look around
         if choice1 == 'look around':
-            print('LOOKING AROUND')
+            print('You take a look around the room.')
+            cont()
+            print('You see that there is a crowbar on the counter to the side.')
+            cont()
+            print('Alternatively, there are some needles on the bedside stand.')
+            input('*')
 
         # Try opening door
         elif choice1 == 'door' or choice1 == 'open door':
-            print('Strangely, the doorknob won\'t budge. It must be locked from the outside.')
+            got_up = True
+            print('You get up from the bed and walk to the door.')
+            cont()
+            print("Strangely, the doorknob won\'t budge. It must be locked from the outside.")
+            cont()
+            print('The door has a window. Maybe you could smash it to reach the lock on the other side?')
+            input('*')
 
         # Smash window w/fist
         elif choice1 in smash_glass:
             punched_window = True
+            if got_up:
+                print('You get up from the bed and walk to the door.')
+                cont()
             print('You smash through the door window with your fist.')
             cont()
             print('Unsurprisingly, you get dozens of cuts from the glass.')
@@ -144,16 +182,16 @@ def intro():
             print('The pain is overwhelming.')
             cont()
             print('...And you eventually die of blood loss.')
-            input("*")
+            input('*')
             game_over()
 
         # Falcon punch window
-        # Player must have tried to smash window w/fist first
+        # (Player must have tried to smash window w/fist first)
         elif punched_window and choice1 in falcon_punch:
-            print("You FALCON PUNCH through the door's window.")
+            print('You FALCON PUNCH through the door\'s window.')
             cont()
-            print("However, Falcon Punches aren't immune to getting cut by glass...")
-            input("*")
+            print('However, Falcon Punches aren\'t immune to getting cut by glass...')
+            input('*')
             game_over()
 
         # Smash window w/crowbar (correct action)
@@ -165,9 +203,9 @@ def intro():
             sequence2()
             break
 
-        # Drugs
+        # Do drugs
         elif choice1 in drugs:
-            print('You reach over to the needles on the counter to your left and violently jab yourself with one.')
+            print('You reach over to the needles on the stand to your left and violently jab yourself with one.')
             cont()
             print('You start to feel high, lighter than air!')
             cont()
@@ -179,9 +217,22 @@ def intro():
             input('*')
             game_over()
 
+        elif choice1 in throw_drugs:
+            if not got_up:
+                print('You reach over to the needles on the stand to your left and throw one at the door window.')
+                cont()
+            elif got_up:
+                print('You go over to the stand by the bed and grab a needle.')
+                cont()
+                print("You throw it at the door's window.")
+                cont()
+            print("Unsurprisingly, it didn't to anything.")
+            # Alternate: needle bounces back and drugs player
+            input('*')
+
         # Hint
         elif choice1 == 'hint':
-            print('HINTS GO HERE')
+            print('Try looking around the room.')
 
         else:
             ifc += 1
